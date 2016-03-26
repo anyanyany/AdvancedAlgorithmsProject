8ec868ef937320e0004e245714f70378a73aaa14
Piotr Waszkiewicz <Waszka23@gmail.com>
2016-03-26 15:50:07 +0100

    Added first graph library files

---
+++ Program/Program/GraphLibrary/Edge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.GraphLibrary
+{
+    /// <summary>
+    /// Represents edge in graph. Each edge has one starting and one ending vertex, 
+    /// each accessible as From and To class fields. Edge can also be undirected 
+    /// which means that each of those two vertices can act as starting and ending points.
+    /// </summary>
+    public class Edge
+    {
+        public bool IsDirected { get; private set; }
+        public uint From { get; private set; }
+        public uint To { get; private set; }
+        public int Weight { get; set; }
+
+        /// <summary>
+        /// Creates edge from provided information. Edge is undirected by default.
+        /// </summary>
+        /// <param name="from">edge starting point</param>
+        /// <param name="to">edge ending point</param>
+        /// <param name="isDirected">information if edge is directed</param>
+        public Edge(uint from, uint to, bool isDirected = false, int weight = 0)
+        {
+            this.From = from;
+            this.To = to;
+            this.IsDirected = isDirected;
+            this.Weight = weight;
+        }
+    }
+}
---
+++ Program/Program/GraphLibrary/MatrixGraph.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program.GraphLibrary
+{
+    class MatrixGraph : IGraph
+    {
+        public uint VerticesCount { get; private set; }
+        private Edge[,] _edgesMatrix;
+
+        public MatrixGraph(uint verticesCount, List<Edge> edges = null)
+        {
+            this.VerticesCount = verticesCount;
+            _edgesMatrix = new Edge[VerticesCount, VerticesCount];
+
+            if (null != edges)
+                foreach (Edge e in edges)
+                    AddEdge(e);
+        }
+
+        public void AddEdge(Edge e)
+        {
+            if (DoesEdgeCollide(e))
+                throw new ArgumentException("Edge collides with another one in the graph");
+            _edgesMatrix[e.From, e.To] = e;
+            if (!e.IsDirected)
+                _edgesMatrix[e.To, e.From] = e;
+        }
+
+        public uint AddVertex()
+        {
+            this.VerticesCount++;
+            Edge[,] tmp = _edgesMatrix;
+            _edgesMatrix = new Edge[VerticesCount, VerticesCount];
+            for (int i = 0; i < VerticesCount - 1; i++)
+                for (int j = 0; j < VerticesCount - 1; j++)
+                    _edgesMatrix[i, j] = tmp[i, j];
+
+            return VerticesCount - 1;
+        }
+
+        public List<Edge> GetEdges(int vertexNumber = -1)
+        {
+            if (vertexNumber != -1)
+                CheckVertexNumberSanity(vertexNumber);
+
+            HashSet<Edge> edgeSet = new HashSet<Edge>();
+            for (int i = 0; i < VerticesCount; i++)
+            {
+                if (vertexNumber != -1 && i != vertexNumber) continue;
+                edgeSet.UnionWith(GetInEdges(i));
+                edgeSet.UnionWith(GetOutEdges(i));
+            }
+
+            return new List<Edge>(edgeSet);
+        }
+
+        public List<Edge> GetInEdges(int vertexNumber)
+        {
+            CheckVertexNumberSanity(vertexNumber);
+
+            List<Edge> edges = new List<Edge>();
+            for (int i = 0; i < VerticesCount; i++)
+                if (_edgesMatrix[i, vertexNumber] != null)
+                    edges.Add(_edgesMatrix[i, vertexNumber]);
+
+            return edges;
+        }
+
+        public List<Edge> GetOutEdges(int vertexNumber)
+        {
+            CheckVertexNumberSanity(vertexNumber);
+
+            List<Edge> edges = new List<Edge>();
+            for (int i = 0; i < VerticesCount; i++)
+                if (_edgesMatrix[vertexNumber, i] != null)
+                    edges.Add(_edgesMatrix[vertexNumber, i]);
+
+            return edges;
+        }
+
+        private bool DoesEdgeCollide(Edge e)
+        {
+            return (_edgesMatrix[e.From, e.To] != null || (!e.IsDirected && _edgesMatrix[e.To, e.From] != null));
+        }
+
+        private void CheckVertexNumberSanity(int vertexNumber)
+        {
+            if (vertexNumber >= VerticesCount || vertexNumber < 0)
+                throw new ArgumentException("Wrong vertex number");
+        }
+    }
+}
